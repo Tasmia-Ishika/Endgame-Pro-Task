550dc57d6d1384d19762e7d0e1c74d3e176d350b
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-07-02 00:15:01 +0600

    new commit

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByOCNLm3CutwrIe7CXP5P5GXExh065dZo",
+  authDomain: "pro-task-endgame.firebaseapp.com",
+  projectId: "pro-task-endgame",
+  storageBucket: "pro-task-endgame.appspot.com",
+  messagingSenderId: "181604128642",
+  appId: "1:181604128642:web:0fa404d1db2b5870d4a19b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
